8a5a8931c241f266518b4e37231ed6452c434197
Zev <zevgeller@gmail.com>
2022-06-09 04:42:52 -0500

    commit tree 412a677176f80689792dacb45cdb27427946b6d5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+//document.querySelector(`p`, function() {
+//    document.querySelector('p').textContent(`KJavaScript is so cool. It lets me add text to my page programmatically.`);    
+//});
+
+document.addEventListener("DOMContentLoaded", function() {
+//    console.log("This is really cool!");
+    document.querySelector('p').textContent = ("This is really cool!");
+});
+
+//DOMContentLoaded
+
+
+//const event = document.createEvent("DOMContentLoaded", e => {
+//    document.dispatchEvent('event');
+
+//});
